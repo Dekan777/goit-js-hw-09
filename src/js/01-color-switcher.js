1d47b5bd314556a0adfa3f91bd9ebc1d32ff2bf3
Denis Koshevyi <denis.koshevoy1987@gmail.com>
2023-11-14 16:08:09 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,34 @@
+const refs = {
+  startBtn: document.querySelector('button[data-start]'),
+  stopBtn: document.querySelector('button[data-stop]'),
+  colorBody: document.body,
+};
 
+let colorRandom;
+let isThemeChanging = false;
+
+refs.startBtn.addEventListener('click', function () {
+  if (!isThemeChanging) {
+    isThemeChanging = true;
+    refs.startBtn.disabled = true;
+    colorRandom = setInterval(setRandomBackgroundColor, 1000);
+  }
+});
+
+refs.stopBtn.addEventListener('click', function () {
+  if (isThemeChanging) {
+    isThemeChanging = false;
+    refs.startBtn.disabled = false;
+    clearInterval(colorRandom);
+  }
+});
+
+function setRandomBackgroundColor() {
+  refs.colorBody.style.backgroundColor = getRandomHexColor();
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
