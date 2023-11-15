ee60c5318f7fc39d974e2535fb98ef2965662d29
Denis Koshevyi <denis.koshevoy1987@gmail.com>
2023-11-16 00:29:15 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,48 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+// const refs = {
+//   form: document.querySelector('.form'),
+//   amountInput: document/querySelector('.input[name="amount"]'),
+//   delayInput: document.querySelector('input[name="delay"]'),
+//   stepInput: document.querySelector('input[name="step"]'),
+
+//   amount: parseInt(refs.amountInput.value), // кількість разів виклику функції
+//   delay: parseInt(refs.delayInput.value), // початкове затримка
+//   step: parseInt(refs.stepInput.value), // різниця між затримками
+// }
+
+
+const form = document.querySelector('.form');
+const amountInput = document.querySelector('input[name="amount"]');
+const delayInput = document.querySelector('input[name="delay"]');
+const stepInput = document.querySelector('input[name="step"]');
+
+
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  const amount = parseInt(amountInput.value); // кількість разів виклику функції
+const delay = parseInt(delayInput.value); // початкове затримка
+const step = parseInt(stepInput.value); // різниця між затримками
+
+
+  for (let i = 0; i < amount; i++) {
+    setTimeout(() => {
+      createPromise(i, delay);
+      delay += step; // збільшуємо затримку для наступного виклику
+    }, delay * i);
+  }
+});
+
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
-    // Fulfill
+    Notify.success(`Fulfilled promise ${position} in ${delay}ms`);
+    
+
   } else {
-    // Reject
+    Notify.failure(`Rejected promise ${position} in ${delay}ms`);
+    
   }
 }
